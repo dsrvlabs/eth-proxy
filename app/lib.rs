794500bdc8316ee07fdd43f4ba9103102865731d
Joonkyo Kim <rootwarp@gmail.com>
2025-02-24 21:31:41 +0900

    Health check and route by round robin

---
+++ app/lib.rs
@@ -0,0 +1,65 @@
+use std::time::{Instant};
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+pub struct Endpoint {
+    pub url: String,
+    pub latency: u64,
+    pub alive: bool,
+}
+
+impl Endpoint {
+    // TODO: Consensus layer health check
+
+    pub async fn execution_layer_health_check(&mut self) {
+        let client = reqwest::Client::new();
+
+        let body = r#"{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}"#;
+
+        let start = Instant::now();
+        let response = client.post(&self.url).body(body).send().await;
+        let duration = start.elapsed();
+
+        match response {
+            Ok(response) => {
+                let body = response.text().await.unwrap_or_default();
+                let json: serde_json::Value = serde_json::from_str(&body).unwrap_or_default();
+                let syncing = json["result"]["syncing"].as_bool().unwrap_or(true);
+                println!("syncing: {} {:?} {:?}", self.url, syncing, duration);
+                self.alive = !syncing;
+            }
+            _ => {
+                println!("syncing: {} false {:?}", self.url, duration);
+                self.alive = false;
+            }
+        }
+    }
+}
+
+pub trait EndpointChooseStrategy: Send + Sync {
+    fn get_endpoint(&mut self) -> Option<&Endpoint>;
+}
+
+pub struct RoundRobinStrategy {
+    endpoints: Vec<Endpoint>,
+    current_index: AtomicUsize,
+}
+
+impl RoundRobinStrategy {
+    pub fn new(endpoints: Vec<Endpoint>) -> Self {
+        Self { endpoints, current_index: AtomicUsize::new(0) }
+    }
+}
+
+impl EndpointChooseStrategy for RoundRobinStrategy {
+    fn get_endpoint(&mut self) -> Option<&Endpoint> {
+        for _ in 0..self.endpoints.len() {
+            let index = self.current_index.fetch_add(1, Ordering::Relaxed);
+            let endpoint = self.endpoints.get(index % self.endpoints.len());
+            if endpoint.unwrap().alive {
+                return Some(endpoint.unwrap());
+            }
+        }
+
+        None
+    }
+}
--- app/server.rs
+++ app/server.rs
@@ -1,24 +1,19 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, HttpRequest};
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::time::Instant;
+
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use reqwest::Client;
 
+use eth_proxy::{Endpoint, EndpointChooseStrategy, RoundRobinStrategy};
 
-// TODOs
-// Periodic health check
-// Maintain liveness
-// Round robin
-
-const servers: [&str; 3] = [
-    "http://34.22.74.19:8547",
-    "http://35.189.142.38:8547",
-    "http://34.87.97.131:8547",
-];
-
-async fn proxy(req: HttpRequest, body: web::Bytes) -> impl Responder {
+async fn proxy(req: HttpRequest, body: web::Bytes, data: web::Data<AppState>) -> impl Responder {
     let client = Client::new();
-    let url = format!("{}{}", servers[0], req.uri());
+
+    let mut strategy = data.strategy.lock().unwrap();
+    let endpoint = strategy.get_endpoint();
 
     println!("headers: {:?}", req.headers());
-    println!("url: {:?}", url);
     println!("method: {:?}", req.method());
     println!("body: {:?}", body);
 
@@ -33,12 +28,22 @@ async fn proxy(req: HttpRequest, body: web::Bytes) -> impl Responder {
         _ => reqwest::Method::GET,
     };
 
-    let mut request = client.request(method, url);
+    if endpoint.is_none() {
+        return HttpResponse::ServiceUnavailable().body("No available endpoints");
+    }
+
+    println!("url: {:?}", endpoint.unwrap().url);
+
+    let mut request = client.request(method, endpoint.unwrap().url.clone());
     for (key, value) in req.headers().iter() {
         request = request.header(key.as_str(), value.as_bytes());
     }
 
+    let start = Instant::now();
     let response = request.body(body).send().await;
+    let duration = start.elapsed();
+
+    println!("Request duration: {:?}", duration);
 
     match response {
         Ok(resp) => {
@@ -56,18 +61,63 @@ async fn proxy(req: HttpRequest, body: web::Bytes) -> impl Responder {
         }
         Err(e) => {
             eprintln!("Error forwarding request: {}", e);
+            // TODO: If failed. does this endpoint need to be failed?
             return HttpResponse::InternalServerError().finish();
         }
     }
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/{tail:.*}", web::route().to(proxy))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+#[derive(Clone)]
+struct AppState {
+    strategy: Arc<Mutex<dyn EndpointChooseStrategy + Send + Sync>>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // TODO: Get from env
+    let servers: [&str; 3] = [
+        "http://34.22.74.19:8547",
+        "http://35.189.142.38:8547",
+        "http://34.87.97.131:8547",
+    ];
+
+    let endpoints: Vec<Endpoint> = servers.iter().map(|s| Endpoint {
+        url: s.to_string(),
+        latency: 0,
+        alive: false,
+    }).collect();
+
+    let mut health_checks = Vec::new();
+
+    for server in servers {
+        let handle = tokio::spawn(async {
+            loop {
+                println!("check");
+                let mut endpoint = Endpoint {
+                    url: server.to_string(),
+                    latency: 0,
+                    alive: false,
+                };
+
+                endpoint.execution_layer_health_check().await;
+
+                // TODO: Periodc
+                std::thread::sleep(std::time::Duration::from_secs(5));
+            }
+        });
+
+        health_checks.push(handle);
+    }
+    let strategy = Arc::new(Mutex::new(RoundRobinStrategy::new(endpoints)));
+    let app_state = AppState { strategy };
+
+    HttpServer::new(move || App::new()
+        .app_data(web::Data::new(app_state.clone()))
+        .route("/{tail:.*}", web::route().to(proxy)))
+        .bind("127.0.0.1:8080")
+        .unwrap()
+        .run()
+        .await?;
+
+    Ok(())
 }
